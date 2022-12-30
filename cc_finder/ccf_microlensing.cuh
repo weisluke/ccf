8995c8c78fe74aaa49c76f0e228e61aefa0f1ab3
Luke Weisenbach <lukeweisenbach@live.com>
2022-12-29 23:40:17 -0500

    updates for taylor approximations

--- cc_finder/ccf_microlensing.cuh
+++ cc_finder/ccf_microlensing.cuh
@@ -103,6 +103,7 @@ approximate lens equation for a rectangular star field
 \param kappastar -- convergence in point mass lenses
 \param corner -- complex number denoting the corner of the
 				 rectangular field of point mass lenses
+\param taylor -- degree of the taylor series for alpha_smooth
 
 \return w = (1 - kappa) * z + gamma * z_bar
 			- theta^2 * sum(m_i / (z - z_i)_bar) - alpha_smooth
@@ -129,16 +130,16 @@ __device__ Complex<T> complex_image_to_source(Complex<T> z, T kappa, T gamma, T 
 
 	for (int i = 1; i <= taylor; i++)
 	{
-		s1 += (z.conj() / corner.conj()).pow(i) / i;
-		s2 += (z.conj() / corner).pow(i) / i;
+		s1 += (z.conj() / corner).pow(i) / i;
+		s2 += (z.conj() / corner.conj()).pow(i) / i;
 		s3 += (z.conj() / -corner).pow(i) / i;
 		s4 += (z.conj() / -corner.conj()).pow(i) / i;
 	}
 
-	Complex<T> alpha_smooth = (-(corner.conj() - z.conj()) * (corner.conj().log() - s1) + (corner - z.conj()) * (corner.log() - s2)
+	Complex<T> alpha_smooth = ((corner - z.conj()) * (corner.log() - s1) - (corner.conj() - z.conj()) * (corner.conj().log() - s2)
 		+ (-corner - z.conj()) * ((-corner).log() - s3) - (-corner.conj() - z.conj()) * ((-corner).conj().log() - s4));
 	alpha_smooth *= Complex<T>(0, -kappastar / PI);
-	alpha_smooth -= kappastar * (corner + corner.conj() + z + z.conj());
+	alpha_smooth -= kappastar * 2 * (corner.re + z.re);
 
 	/*(1-kappa)*z+gamma*z_bar-starsum_bar-alpha_smooth*/
 	return (1 - kappa) * z + gamma * z.conj() - starsum.conj() - alpha_smooth;
